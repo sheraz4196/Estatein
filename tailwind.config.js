a35f4b9c8e73bc9f50948cd0e25e0bf1c4bb6a62
hassnainzia121 <hassnainzia.dev121@gmail.com>
2024-09-20 02:55:58 +0500

    create animation for index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,71 @@ module.exports = {
   content: ["./*.html", "./src/*.html"],
   theme: {
     extend: {
+      keyframes: {
+        appear: {
+          "0%": {
+            opacity: "0",
+            "clip-path": "inset(100% 100% 0 0)",
+          },
+          "100%": {
+            opacity: "1",
+            "clip-path": "inset(0 0 0 0)",
+          },
+        },
+        fadeIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-200px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+          },
+        },
+        fadeOut: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(200px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+          },
+        },
+        flowerOpen: {
+          "0%": {
+            transform: "scale(0.2) rotate(0deg)", // Start small and unrotated
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1) rotate(0deg)", // Settle at full size and final position
+            opacity: "1",
+          },
+        },
+        "slide-left": {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-right": {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-bottom": {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        appear: "appear linear 1s",
+        fadeIn: "fadeIn linear 1s",
+        fadeOut: "fadeOut linear 1s",
+        flowerOpen: "flowerOpen 2s ease-in-out forwards",
+        "slide-left": "slide-left 1s forwards", // Adjust duration as needed
+        "slide-right": "slide-right 1s forwards", // Adjust duration as needed
+        "slide-bottom": "slide-bottom 1s forwards",
+        // "animation-timeline": "view()",
+        // "animation-range": " entry 0% cover 40%",
+      },
       boxShadow: {
         "custom-light": "2px 4px 16px -1px rgba(90,90,90,0.3)", // Light color shadow
         "custom-dark": "0 4px 6px -1px rgba(0, 0, 0, 0.75)", // Dark color shadow
@@ -50,7 +115,13 @@ module.exports = {
         },
       };
 
-      addUtilities(newUtilities, ["responsive", "hover"]);
+      // Add your scroll-animate utility here
+      addUtilities({
+        ".scroll-animate": {
+          "animation-timeline": "view()",
+          "animation-range": "entry 0% cover 40%",
+        },
+      });
     },
   ],
 };
