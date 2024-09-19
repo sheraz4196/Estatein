1a9ca18fa3f455fd5509b397057ec4d0e6d874db
hassnainzia121 <hassnainzia.dev121@gmail.com>
2024-09-20 04:16:22 +0500

    complete animation in about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,6 +48,10 @@ module.exports = {
           "0%": { transform: "translateX(-100%)", opacity: "0" },
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
+        "slide-top": {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
         "slide-right": {
           "0%": { transform: "translateX(100%)", opacity: "0" },
           "100%": { transform: "translateX(0)", opacity: "1" },
@@ -56,15 +60,27 @@ module.exports = {
           "0%": { transform: "translateY(100%)", opacity: "0" },
           "100%": { transform: "translateY(0)", opacity: "1" },
         },
+        "circle-Open": {
+          "0%": {
+            opacity: "0",
+            "clip-path": "circle(0% at 50% 50%)",
+          },
+          "100%": {
+            opacity: "1",
+            "clip-path": "circle(100% at 50% 50%)",
+          },
+        },
       },
       animation: {
-        appear: "appear linear 1s",
-        fadeIn: "fadeIn linear 1s",
-        fadeOut: "fadeOut linear 1s",
-        flowerOpen: "flowerOpen 2s ease-in-out forwards",
-        "slide-left": "slide-left 1s forwards", // Adjust duration as needed
-        "slide-right": "slide-right 1s forwards", // Adjust duration as needed
-        "slide-bottom": "slide-bottom 1s forwards",
+        appear: "appear linear 2s",
+        fadeIn: "fadeIn linear 2s",
+        fadeOut: "fadeOut linear 2s",
+        flowerOpen: "flowerOpen 2s forwards",
+        "slide-left": "slide-left 2s forwards", // Adjust duration as needed
+        "slide-right": "slide-right 2s forwards", // Adjust duration as needed
+        "slide-bottom": "slide-bottom 2s forwards",
+        "circle-open": "circle-Open 2s forwards",
+        "slide-top": "slide-top 2s forwards",
         // "animation-timeline": "view()",
         // "animation-range": " entry 0% cover 40%",
       },
@@ -113,15 +129,14 @@ module.exports = {
           borderRadius: "inherit",
           zIndex: "-1",
         },
+        ".scroll-animate": {
+          animationTimeline: "view()",
+          animationRange: "entry 0% cover 40%",
+        },
       };
 
-      // Add your scroll-animate utility here
-      addUtilities({
-        ".scroll-animate": {
-          "animation-timeline": "view()",
-          "animation-range": "entry 0% cover 40%",
-        },
-      });
+      // Add utilities with responsive and hover variants
+      addUtilities(newUtilities, ["responsive", "hover"]);
     },
   ],
 };
